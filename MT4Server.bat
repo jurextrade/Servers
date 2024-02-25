6c0d539d3f4edffc78d43ce40a696a30db5e1b21
jurex <40755828+jurextrade@users.noreply.github.com>
2024-02-25 18:37:20 +0100

    Add files via upload

---
+++ MT4Server.bat
@@ -0,0 +1,2 @@
+cd C:\PROJECTS_CROSS\MT4Progress\NewWeb\Servers\
+node C:\PROJECTS_CROSS\MT4Progress\NewWeb\Servers\MT4server.js 
---
+++ NetprogServer.bat
@@ -0,0 +1,2 @@
+cd C:\PROJECTS_CROSS\MT4Progress\newweb\Servers
+node C:\PROJECTS_CROSS\MT4Progress\newweb\Servers\NetprogServer.js 
